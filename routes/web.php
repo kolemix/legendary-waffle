ecc3f625c20ce3ebeb2ea6db770100b72348c473
KhoiNguyen-konu <030239230153@st.buh.edu.vn>
2026-03-18 15:47:00 +0700

    phannguyenkhoinguyen

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get("/qlsach/theloai", "App\Http\Controllers\BookController@laythongtintheloai");
 Route::get("/qlsach/thongtinsach", "App\Http\Controllers\BookController@laythongtinsach");
 Route::get("/qlsach/kinhdien", "App\Http\Controllers\BookController@sachkinhdien");
+
+Route::get('/phannguyenkhoinguyen', function () {
+    return "Phan Nguyễn Khôi Nguyên";
+});
