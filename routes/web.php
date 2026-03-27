e5818b3414cf145b8b2e9de2b14dcac3644c9d94
030239230035-stack <030239230035@st.buh.edu.vn>
2026-03-27 16:18:25 +0700

    them trang danh sach sach va theo the loai

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,18 @@ Route::get('/', function () {
 Route::get('/doanphucgiakhanh', function () {
     return 'Doan Phuc Gia Khanh';
 });
+Route::get('/nguyentuandung', function () {
+    return 'Nguyen Tuan Dung';
+});
+Route::get('/sach', function () {
+    $sach = DB::table('sach')->get();
+    $theloai = DB::table('dm_the_loai')->get();
+    return view('danh_sach_sach', compact('sach', 'theloai'));
+});
+
+Route::get('/theloai/{id}', function ($id) {
+    $sach = DB::table('sach')->where('the_loai', $id)->get();
+    $theloai = DB::table('dm_the_loai')->get();
+    $tentheloai = DB::table('dm_the_loai')->where('id', $id)->first();
+    return view('danh_sach_sach', compact('sach', 'theloai', 'tentheloai'));
+});
